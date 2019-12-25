e28373f29fd7d66f72918d3f6d36fcecde4534c3
tjbulick <e.pichtchalnikov@gmail.com>
2019-12-25 16:55:46 +0300

    Updates

---
+++ precache-manifest.e4c1e8c2bc75590f5ef000d603504aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9898df5aac047c32a1e5c024e1be90",
+    "url": "/react-sea-battle/index.html"
+  },
+  {
+    "revision": "144e7ab0901cda8c07fc",
+    "url": "/react-sea-battle/static/css/main.063da00f.chunk.css"
+  },
+  {
+    "revision": "ee8602b665573605d849",
+    "url": "/react-sea-battle/static/js/2.8db5d7df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-sea-battle/static/js/2.8db5d7df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "144e7ab0901cda8c07fc",
+    "url": "/react-sea-battle/static/js/main.749e5b36.chunk.js"
+  },
+  {
+    "revision": "218a2f05aa7d8bdd651d",
+    "url": "/react-sea-battle/static/js/runtime-main.bdc71014.js"
+  }
+]);
